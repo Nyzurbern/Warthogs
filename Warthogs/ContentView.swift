d9fcdceed3ffe9c894c2b633d2d23bced18a8f13
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-16 10:53:20 +0800

    Added title to main menu as well as basic hand pose detction (not tested)

--- Warthogs/ContentView.swift
+++ Warthogs/ContentView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Text("Warthogs")
+                .font(.largeTitle)
         }
         .padding()
+        .background(.red)
+        .scaledToFill()
+        .ignoresSafeArea()
     }
 }
 
