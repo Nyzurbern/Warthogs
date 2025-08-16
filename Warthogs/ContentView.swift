60133aff34501faf188e7e45a15702e709aaeb21
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-16 11:57:56 +0800

    View changes to make it better

--- Warthogs/ContentView.swift
+++ Warthogs/ContentView.swift
@@ -10,27 +10,26 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            
             Image("mercury")
                 .resizable()
                 .aspectRatio(contentMode: .fill)
                 .frame(width: 400, height: 200, alignment: .top)
             
-        }
-        
-        VStack {
-            
             Text("Warthogs")
                 .font(.largeTitle)
-            
-            Image("playButton")
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 60, height: 80, alignment: .top)
-            
+            Button {
+                print("Yay")
+            } label: {
+                VStack{
+                    Image("playButton")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 60, height: 60, alignment: .top)
+                }
+            }
         }
-        
-        
+        .aspectRatio(contentMode: .fill)
+        .frame(width: 1000, height: 80, alignment: .centerFirstTextBaseline)
         .padding()
         .ignoresSafeArea()
     }
