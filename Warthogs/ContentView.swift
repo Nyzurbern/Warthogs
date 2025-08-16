10fde555e354a3e70790cd2d4fb2c34c33a6959a
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-08-16 11:37:06 +0800

    hello i put plat button and mercury image

--- Warthogs/ContentView.swift
+++ Warthogs/ContentView.swift
@@ -9,13 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
+        VStack {
+            
+            Image("mercury")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 400, height: 200, alignment: .top)
+            
+        }
+        
+        VStack {
+            
             Text("Warthogs")
                 .font(.largeTitle)
+                .font(<#T##font: Font?##Font?#>)
+            
+            Image("playButton")
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 60, height: 80, alignment: .top)
+            
         }
+        
+        
         .padding()
-        .background(.red)
-        .scaledToFill()
         .ignoresSafeArea()
     }
 }
