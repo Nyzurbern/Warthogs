45ce7ab109a167e83736cff8e1fcdcd37c83a2e9
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-23 11:39:17 +0800

    Timer View Visuals completed

--- Warthogs/GameView/GameView.swift
+++ Warthogs/GameView/GameView.swift
@@ -14,10 +14,14 @@ struct GameView: View {
     
     @State private var handPoseInfo: String = "Detecting hand poses..."
     @State private var handPoints: [CGPoint] = []
+    
     @State private var EnemyScore: Int = 0
+    @State private var PlayerScore: Int = 0
+    @State private var timer: String = "5:00"
     
     let timerColour: Color = Color(red: 0.9764 ,green: 0.8078, blue: 0.8078)
     let enemyScoreColour: Color = Color(red: 0.9764, green: 0, blue: 0)
+    let playerScoreColour: Color = Color(red: 0.3961, green: 0.8588, blue: 0.8078)
     
     var body: some View {
         
@@ -26,8 +30,16 @@ struct GameView: View {
                 .frame(width: 600, height: 60, alignment: .top)
                 .foregroundStyle(timerColour)
             HStack{
-                Text("0")
+                Text(String(PlayerScore))
                     .font(.largeTitle)
+                    .foregroundStyle(playerScoreColour)
+                    .offset(x: -225, y: 0)
+                Text(timer)
+                    .font(.largeTitle)
+                Text(String(EnemyScore))
+                    .font(.largeTitle)
+                    .foregroundStyle(enemyScoreColour)
+                    .offset(x: 225, y: 0)
             }
         }
         .offset(x: 0, y: 20)
