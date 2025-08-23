26bfc092b1bda0884327db526a355b7d8d559841
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-08-23 09:43:27 +0800

    put text box for name.. again...

--- Warthogs/LoginView.swift
+++ Warthogs/LoginView.swift
@@ -5,3 +5,26 @@
 //  Created by T Krobot on 23/8/25.
 //
 
+import SwiftUI
+
+struct LoginView: View {
+    
+    @State private var textInput = ""
+    
+    var body: some View {
+        
+        VStack {
+            
+            Text("Your name here")
+                .font(.largeTitle)
+            
+            TextField("Name", text: $textInput)
+                .onSubmit {
+                    
+                    print(textInput)
+                }
+            
+        
+        }
+    }
+}
