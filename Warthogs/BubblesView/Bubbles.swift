cb7270a3188eb47377c20e3c4ecc93010e0a83f1
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-30 10:40:00 +0800

    New bubble struct and view wip for collision detection

---
+++ Warthogs/BubblesView/Bubbles.swift
@@ -0,0 +1,13 @@
+//
+//  Bubbles.swift
+//  Warthogs
+//
+//  Created by T Krobot on 30/8/25.
+//
+import SwiftUI
+
+struct Bubble: Identifiable {
+    let id = UUID()
+    let name: String
+    var image: String
+}
