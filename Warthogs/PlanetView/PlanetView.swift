2050b1448f1718b207570ad9ca7a2de6c8ab5356
Khoo Zi Qi <qiqicool7788@gmail.com>
2025-08-30 12:24:20 +0800

    linked planet to character. need to add varaibles to the mars and venus selection.

--- Warthogs/PlanetView/PlanetView.swift
+++ Warthogs/PlanetView/PlanetView.swift
@@ -13,42 +13,48 @@ struct PlanetView: View {
     var body: some View {
         
         Text("Pick your planet!")
-            
+        
             .font(.system(size: 50))
             .multilineTextAlignment(.leading)
             .padding(0.2)
         
         HStack {
             
-            VStack {
-                Image("mars")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 290, height: 250, alignment: .leading)
-                    .padding()
-                Text("Mars")
-                    .font(.system(size: 25))
-                    .fontWeight(.bold)
-            
+            Button(action: {
+                showingFullScreen = true
+            }) {
+                VStack{
+                    Image("mars")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 290, height: 250, alignment: .leading)
+                        .padding()
+                    Text("Mars")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                }
+                
+                VStack {
+                    Image("venus")
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: 290, height: 220, alignment: .leading)
+                        .padding()
+                    Text(".")
+                        .font(.system(size: 15))
+                    Text("Venus")
+                        .font(.system(size: 25))
+                        .fontWeight(.bold)
+                }
             }
-            
-            VStack {
-                Image("venus")
-                    .resizable()
-                    .aspectRatio(contentMode: .fill)
-                    .frame(width: 290, height: 220, alignment: .leading)
-                    .padding()
-                Text(".")
-                    .font(.system(size: 15))
-                Text("Venus")
-                    .font(.system(size: 25))
-                    .fontWeight(.bold)
+            .background(.orange)
+            .fullScreenCover(isPresented: $showingFullScreen) {
+                CharacterInfoView()
             }
         }
-        .background(.orange)
     }
 }
-    
+        
         #Preview {
-                PlanetView()
+            PlanetView()
         }
