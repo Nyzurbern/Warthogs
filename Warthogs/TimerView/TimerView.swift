04549b86d9ce5175e71221940c9b1d641bb15259
Nyzurbern <Hrithik_tec@outlook.sg>
2025-08-30 12:08:14 +0800

    Fixed Timer View bug causing black bar appearing at the top

--- Warthogs/TimerView/TimerView.swift
+++ Warthogs/TimerView/TimerView.swift
@@ -8,8 +8,34 @@
 import SwiftUI
 
 struct TimerView: View {
+    @State private var EnemyScore: Int = 0
+    @State private var PlayerScore: Int = 0
+    @State private var timer: String = "5:00"
+    
+    let timerColour: Color = Color(red: 0.9764 ,green: 0.8078, blue: 0.8078)
+    let enemyScoreColour: Color = Color(red: 0.9764, green: 0, blue: 0)
+    let playerScoreColour: Color = Color(red: 0.3961, green: 0.8588, blue: 0.8078)
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .frame(width: 600, height: 60, alignment: .top)
+                .foregroundStyle(timerColour)
+                .border(.black, width: 2)
+            HStack{
+                Text(String(PlayerScore))
+                    .font(.largeTitle)
+                    .foregroundStyle(playerScoreColour)
+                    .offset(x: -225, y: 0)
+                Text(timer)
+                    .font(.largeTitle)
+                Text(String(EnemyScore))
+                    .font(.largeTitle)
+                    .foregroundStyle(enemyScoreColour)
+                    .offset(x: 225, y: 0)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
     }
 }
 
